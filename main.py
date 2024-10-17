7216541e3f84d061c0a0179976718697615697f7
Hazim Al-Abdalla <hazimabd2@gmail.com>
2024-10-17 03:29:49 +0200

    feat: added 3d visualization to OCT scans

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
-from utils import normalize, show_scan, import_oct, interpolate_scan, align_oct
+from utils import *
 
 # Shape: [25, 496, 512] - 25 slices of 496x512
 file_path = "eye.e2e"
 scan = import_oct(file_path)
 print("Original shape:", scan.shape)
-show_scan(scan, title="Original")
+show_slices(scan, title="Original")
 
-scan = align_oct(scan)
-show_scan(scan, title="Aligned")
+scan = align_oct(scan, special_slice=1)
+print(scan.shape)
+show_slices(scan, title="Aligned")
 
 scan = interpolate_scan(scan, method='linear')
 print("Interpolated shape:", scan.shape)
-show_scan(scan, x=5, y=10, title="Aligned + Interpolated")
+show_slices(scan, x=5, y=10, title="Aligned + Interpolated")
 
 scan = normalize(scan)
+scan = segment_eye_components(scan)
+plot_oct_3d(scan)
--- utils.py
+++ utils.py
@@ -1,24 +1,24 @@
+import cv2
 import matplotlib.pyplot as plt
 import eyepy as ep
+import skimage
 import torch
 import numpy as np
 from scipy.interpolate import RegularGridInterpolator
-from scipy.ndimage import center_of_mass
+from scipy.ndimage import center_of_mass, minimum_filter
 
 
 def import_oct(path):
-    # Load the e2e file as an EyeVolume object
     volume = ep.import_heyex_e2e(path)
     volume_data = torch.tensor(volume.data, dtype=torch.float32)
     return volume_data
 
 
 def normalize(data):
-    #return data / 255.0
-    return (data - data.min()) / (data.max() - data.min())
+    return data / 255.0
 
 
-def show_scan(scan, x=5, y=5, title='plot'):
+def show_slices(scan, x=5, y=5, title='plot'):
     fig, axes = plt.subplots(x, y, figsize=(10, 10))
     axes = axes.ravel()
 
@@ -52,33 +52,23 @@ def interpolate_scan(scan, method='linear'):
     return  torch.tensor(interpolator((depth_grid, height_grid, width_grid)), dtype=torch.float32)
 
 
-def align_oct(oct_tensor, reference_slice_index=12, threshold=0):
+def align_oct(oct_tensor, reference_slice_index=12, threshold=0, special_slice=None):
+    # special slice is written in number not index
+    if special_slice is not None:
+        special_slice -= 1
+
     num_slices, height, width = oct_tensor.shape
     aligned_slices = []
 
-    reference_slice = oct_tensor[reference_slice_index].numpy()
+    reference_slice = np.array(oct_tensor[reference_slice_index])
     reference_mask = reference_slice > threshold
     reference_com = center_of_mass(reference_mask)
 
-    # Special handling for slice 1 as it is always shifted higher than the rest of the slices
-    slice_1 = oct_tensor[0].numpy()
-    com_y_1, com_x_1 = center_of_mass(slice_1)
-    center_y = height // 2
-    shift_y_1 = int(center_y - com_y_1)
-
-    shifted_slice_1 = np.zeros_like(slice_1)
-    if shift_y_1 > 0:
-        shifted_slice_1[shift_y_1:, :] = slice_1[:height - shift_y_1, :]
-    elif shift_y_1 < 0:
-        shifted_slice_1[:height + shift_y_1, :] = slice_1[-shift_y_1:, :]
-    else:
-        shifted_slice_1 = slice_1
-
-    aligned_slices.append(shifted_slice_1)
-
-    # Rest of the slices, so we start from 2nd slice
-    for i in range(1, oct_tensor.shape[0]):
-        slice_ = oct_tensor[i].numpy()
+    # Handle the rest of the slices first, excluding the special slice
+    for i in range(num_slices):
+        if i == special_slice:
+            continue
+        slice_ = np.array(oct_tensor[i])
 
         mask = slice_ > threshold
         com = center_of_mass(mask)
@@ -89,6 +79,65 @@ def align_oct(oct_tensor, reference_slice_index=12, threshold=0):
         aligned_slice = np.roll(slice_, shift=(shift_x, shift_y), axis=(0, 1))
         aligned_slices.append(aligned_slice)
 
+    if special_slice is not None:
+        slice_ = np.array(oct_tensor[special_slice])
+        com_y, com_x = center_of_mass(slice_)
+        center_y = height // 2
+        shift_y = int(center_y - com_y)
+
+        shifted_slice = np.zeros_like(slice_)
+        if shift_y > 0:
+            shifted_slice[shift_y:, :] = slice_[:height - shift_y, :]
+        elif shift_y < 0:
+            shifted_slice[:height + shift_y, :] = slice_[-shift_y:, :]
+        else:
+            shifted_slice = slice_
+
+        aligned_slices.insert(special_slice, shifted_slice)
+
     aligned_tensor = np.stack(aligned_slices)
 
     return torch.tensor(aligned_tensor, dtype=torch.float32)
+
+
+def segment_eye_components(tensor):
+    tensor = np.array(tensor)
+    segmented_tensor = np.zeros_like(tensor)
+
+    for i in range(tensor.shape[0]):
+        slice_img = tensor[i]
+        smoothed_slice = minimum_filter(slice_img, size=(2, 2))
+
+        _, binary_img = cv2.threshold(smoothed_slice, np.mean(slice_img) * 0.5, 255, cv2.THRESH_BINARY)
+
+        labels = skimage.measure.label(binary_img, connectivity=2)
+
+        props = skimage.measure.regionprops(labels)
+        if props:
+            largest_component = max(props, key=lambda x: x.area)
+            mask = labels == largest_component.label
+            segmented_tensor[i][mask] = tensor[i][mask]
+
+    segmented_tensor = align_oct(segmented_tensor)
+    return segmented_tensor
+
+
+def plot_oct_3d(tensor):
+    mask = tensor > 0.0
+    x, y, z = np.indices(tensor.shape)
+
+    x_nonzero = x[mask]
+    y_nonzero = y[mask]
+    z_nonzero = z[mask]
+    values_nonzero = tensor[mask]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(x_nonzero, y_nonzero, z_nonzero, c=values_nonzero, cmap='gray', s=2)
+    ax.set_xlim(0, tensor.shape[0])
+    ax.set_ylim(0, tensor.shape[1])
+    ax.set_zlim(0, tensor.shape[2])
+    ax.view_init(roll=100, azim=195)
+    ax.set_axis_off()
+    plt.show()
